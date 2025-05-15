30e388763ed9e1dc5e59ac02246de4427640b7c7
MilanIsac <milan.isac.7@gmail.com>
2025-05-15 22:44:51 +0530

    db initialization

--- signsync_backend/instance/config.py
+++ signsync_backend/instance/config.py
@@ -1,3 +1,3 @@
 # Sensitive configuration
-SECRET_KEY = 'your-secret-key-here'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:your_password@localhost/signsync_db'
+SECRET_KEY = 'f8231409819439f9d877e38740e9d3ef962e63dbae391203bffe10ae35948d0c'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Milanpass77@localhost/signsync_db'
